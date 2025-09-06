d9152f5fa2c4457c9d6946b2f42f9743b27cf222
Nandini0409 <nandinidixit0409@gmail.com>
2025-09-06 23:04:50 +0530

    Redesigned the dashboard UI

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,17 +1,17 @@
-const Header = (showName) => {
+const Header = ({showName}) => {
   return (
-    <header className="bg-[#FAF5E9] relative px-9 py-4">
+    <header className="bg-[#FAF5E9] relative py-4">
       <div className="absolute inset-0 bg-[#FFEDD5]/40 "></div>
 
       <nav className="relative z-10">
         <ul className="flex justify-between items-center sm:px-6 text-[white]">
           <li>
-            <img src="/images/logo3.png" width={100} alt="DevBoard Logo" />
+            <img src="/images/logo3.png" width={80} alt="DevBoard Logo" />
           </li>
           <li>
             {showName===true
               ? <p>Nandini Dixit</p>
-              : <button className="bg-[#1b5748] hover:bg-[#316961e7] text-white rounded-full text-lg font-bold shadow-lg hover:shadow-md transition w-30 py-2">Signup</button>
+              : <button className="bg-[#345f54] hover:bg-[#316961e7] text-white rounded-full text-md shadow-lg hover:shadow-md transition w-25 py-1.5">Signup</button>
             }
           </li>
         </ul>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,80 +4,84 @@ import AboutArtisan from '../components/sections/DashboardSection/aboutArtisan'
 import SocialLinks from '../components/sections/DashboardSection/addSocials'
 import AddArtworks from '../components/sections/DashboardSection/addArtworks'
 import Header from '../components/layouts/Header'
+import { HomeIcon } from '@heroicons/react/24/solid';
+
 
 const Dashboard = () => {
-  const [formType, setFormType]=useState('profile')
+  const [formType, setFormType] = useState('artworks')
   return (
     <>
-    {/* <div className='bg-[#fcf2e1]'>
-      <Header showName={true}/>
-      <div className='flex'>
-      <section className='flex flex-col w-[20vw] h-[100vh] items-center gap-4 px-5 py-7' id="sidebar">
-        <a className='bg-white w-[100%] shadow-lg text-center py-4 rounded-xl' href='#' onClick={()=>{setFormType('profile')}}>About me</a>
-        <a className='bg-white w-[100%] shadow-lg text-center py-4 rounded-xl' href='#' onClick={()=>{setFormType('artwork')}}>Artwork</a>
-        <a className='bg-white w-[100%] shadow-lg text-center py-4 rounded-xl' href='#' onClick={()=>{setFormType('socials')}}>Contact & Socials</a>
-      </section>
-      {formType==='profile'?<AboutArtisan />:null}
-      {formType==='artwork'?<AddArtworks />:null}
-      {formType==='socials'?<SocialLinks/>:null}
-      </div>
-    </div> */}
-<>
-  <div className="bg-white min-h-screen">
-
-    <div className="flex">
-      {/* Sidebar */}
-      <section
-        id="sidebar"
-        className="flex flex-col w-[16vw] h-[100vh] bg-[#fffaf3] border-r border-[#f0e0c9] shadow-md px-4 py-6 gap-4"
-      >
-        <a href='/'><img className='mx-auto' src="/images/logo3.png" width={100} alt="EAZL logo" /></a>
-        <p className='bg-white w-full shadow-sm text-center py-3 rounded-lg font-medium text-gray-700 hover:bg-[#ffe9d6] hover:shadow-md transition'>Nandini Dixit</p>
-        <a
-          className="bg-white w-full shadow-sm text-center py-3 rounded-lg font-medium text-gray-700 hover:bg-[#ffe9d6] hover:shadow-md transition"
-          href="#"
-          onClick={() => {
-            setFormType("profile");
-          }}
-        >
-          About Me
-        </a>
-        <a
-          className="bg-white w-full shadow-sm text-center py-3 rounded-lg font-medium text-gray-700 hover:bg-[#ffe9d6] hover:shadow-md transition"
-          href="#"
-          onClick={() => {
-            setFormType("artwork");
-          }}
-        >
-          Artwork
-        </a>
-        <a
-          className="bg-white w-full shadow-sm text-center py-3 rounded-lg font-medium text-gray-700 hover:bg-[#ffe9d6] hover:shadow-md transition"
-          href="#"
-          onClick={() => {
-            setFormType("socials");
-          }}
-        >
-          Contact & Socials
-        </a>
+    <div className='flex w-[100vw] justify-between '>
+      <section id="left" className="w-[15%] py-3 h-[100vh] border-l rounded-xl flex flex-col justify-around items-center text-center">
+        <div id="logo">
+          <a href='/'>
+            <img width={70} src="/images/logo3.png" alt="logo" />
+          </a>
+        </div>
+        <div id="updateLinks" className='flex flex-col'>
+          <a href='#' onClick={()=>{setFormType('artworks')}}><HomeIcon />Dashboard</a>
+          <a href='#' onClick={() => { setFormType('profile') }}>About me</a>
+          <a href='#' onClick={() => { setFormType('artworkForm') }}>Artwork</a>
+          <a href='#' onClick={() => { setFormType('socials') }}>Contact & Socials</a>
+        </div>
+        <div id="helpLink" className='flex flex-col justify-center items-center'>
+          <a href='/'>
+            <img width={120} src="/images/helpCenterIcon2.png" alt="help icon" />
+          </a>
+          <p className='leading-0.3 text-[12px] px-4 text-gray-500'>Do you have any problem while using EAZL?</p>
+        </div>
       </section>
 
-      {/* Content Area */}
-      <main className="flex-1 px-5 overflow-y-auto">
-        {formType === "profile" ? <AboutArtisan /> : null}
-        {formType === "artwork" ? <AddArtworks /> : null}
-        {formType === "socials" ? <SocialLinks /> : null}
-      </main>
+
+
+
+      <section id="center" className='border-l rounded-xl w-[60%] bg-[#dbd2b533]'>
+        {formType === 'artworks'? <div id="cardContainer">
+          <div>
+            <img src="/images/artwork1.png" alt="title" />
+            <p>title</p>
+            <p>description</p>
+          </div>
+          <div>
+            <img src="/images/artwork2.png" alt="title" />
+            <p>title</p>
+            <p>description</p>
+          </div>
+          <div>
+            <img src="/images/artwork3.png" alt="title" />
+            <p>title</p>
+            <p>description</p>
+          </div></div>:null}
+        
+        
+        <p>placehoder or user's uploaded art.</p>
+        {formType === 'profile' ? <AboutArtisan /> : null}
+        {formType === 'artworkForm' ? <AddArtworks /> : null}
+        {formType === 'socials' ? <SocialLinks /> : null}
+      </section >
+
+
+
+      <section id="right" className='border-l rounded-xl w-[25%]'>
+        <div id="userProfile">
+          <img src="/images/userImage.png" alt="userImage" />
+          <p>Name</p>
+          <p>@username</p>
+          <p>about user</p>
+          <button>Update profile</button>
+          <p>links user have added in portfolio</p>
+          <button>Update links</button>
+        </div>
+        <div id="analytics">
+          <p>portfolio analytics</p>
+        </div>
+      </section>
     </div>
-  </div>
-</>
-
     </>
   )
 }
 
 export default Dashboard
-
 
 
 
